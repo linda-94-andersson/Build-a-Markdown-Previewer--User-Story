f0ad816fa7c27e51eae514ec47d7dbdb24ce6d36
Linda Andersson <linda.29.andersson@gmail.com>
2023-07-25 13:31:21 +0200

    Setup for innerHtml and makred

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,5 +1,19 @@
-const Preview = () => {
-  return <div id="preview">Preview</div>;
+import React, { useState, useEffect } from "react";
+
+const Preview = ({ markdown }) => {
+  const [previewHTML, setPreviewHTML] = useState("");
+
+  useEffect(() => {
+    const renderer = new window.marked.Renderer();
+    renderer.link = (href, title, text) => {
+      return `<a target="_blank" href="${href}">${text}</a>`;
+    };
+
+    // Update previewHTML whenever markdown changes
+    setPreviewHTML(window.marked(markdown, { renderer }));
+  }, [markdown]); // Only markdown as dependency
+
+  return <div id="preview" dangerouslySetInnerHTML={{ __html: previewHTML }} />;
 };
 
 export default Preview;
