d5ac9cefc03d787fb1a90799409879290e242078
Linda Andersson <linda.29.andersson@gmail.com>
2023-07-25 11:24:00 +0200

    Render of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Editor from "./components/Editor";
+import Preview from "./components/Preview";
 
 function App() {
-  return <>App</>;
+  return (
+    <div className="app">
+      <Editor />
+      <Preview />
+    </div>
+  );
 }
 
 export default App;
