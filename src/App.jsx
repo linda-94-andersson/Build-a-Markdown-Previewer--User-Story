ef97243199bd80e2793cfc2504ac82ff8c97ad75
Linda Andersson <linda.29.andersson@gmail.com>
2023-07-25 13:30:34 +0200

    State handinling in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
+import React, { useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import Preview from "./components/Preview";
 
 function App() {
+  const defaultMarkdown = `# Heading
+## Sub Heading
+[Link](https://www.example.com)
+
+\`inline code\`
+
+\`\`\`
+// Code block
+function example() {
+  return 'Hello, world!';
+}
+\`\`\`
+
+- List item 1
+- List item 2
+
+> Blockquote
+
+![Image](https://www.example.com/image.jpg)
+
+**Bolded text**
+`;
+
+  const [markdown, setMarkdown] = useState(defaultMarkdown);
+
+  const handleEditorChange = (value) => {
+    setMarkdown(value);
+  };
+
   return (
     <div className="app">
-      <Editor />
-      <Preview />
+      <Editor value={markdown} onChange={handleEditorChange} />
+      <Preview markdown={markdown} />
     </div>
   );
 }
