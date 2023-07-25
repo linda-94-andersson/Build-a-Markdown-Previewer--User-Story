1c0b1e089f9ec0037653fc891481e3ed005fd6de
Linda Andersson <linda.29.andersson@gmail.com>
2023-07-25 13:30:58 +0200

    Props of value and change

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,5 +1,19 @@
-const Editor = () => {
-  return <textarea id="editor">Editor</textarea>;
+import React from "react";
+
+const Editor = ({ value, onChange }) => {
+  const handleChange = (event) => {
+    onChange(event.target.value);
+  };
+
+  return (
+    <textarea
+      id="editor"
+      rows="33"
+      cols="55"
+      value={value}
+      onChange={handleChange}
+    ></textarea>
+  );
 };
 
 export default Editor;
